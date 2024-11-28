f44c0b047393b0ea4790b4ed41e17ebeb5e82824
x2ketarol <guhming217@mail.ru>
2024-11-28 21:28:53 +0300

    unlucky box

---
+++ Source/RatageddonAR/Assets/!Scripts/LuckyBox.cs
@@ -0,0 +1,71 @@
+using DG.Tweening;
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class LuckyBox : MonoBehaviour, IInteractable
+{
+    public event Action OnExplode;
+    [SerializeField] private float _shakeThreshold = 2f;
+    [SerializeField] private float _shakeCooldown = 1f;
+    [SerializeField, Min(1)] private int _shakesForExplode = 3;
+    [SerializeField] private ParticleSystem _explosion;
+
+    private Vector3 _lastAcceleration;
+    private float _lastShakeTime;
+
+    private bool _isTaken = false;
+    public void Interact(Player interactor)
+    {
+        _lastAcceleration = Input.acceleration;
+        _lastShakeTime = Time.time;
+        if (!_isTaken)
+        {
+            _isTaken = true;
+            StartCoroutine(Translate(interactor));
+        }
+        Debug.Log("INTERACT");
+    }
+
+    private IEnumerator Translate(Player interactor)
+    {
+        transform.DOMove(interactor.ItemAnchor.position, 0.4f).SetEase(Ease.InOutSine);
+        while (DOTween.IsTweening(transform))
+            yield return null;
+        transform.SetParent(interactor.ItemAnchor);
+    }
+
+    private void Update()
+    {
+        Vector3 currentAcceleration = Input.acceleration;
+        float accelerationDelta = (currentAcceleration - _lastAcceleration).magnitude;
+
+        if (accelerationDelta > _shakeThreshold && Time.time > _lastShakeTime + _shakeCooldown)
+        {
+            Handheld.Vibrate();
+            _shakesForExplode--;
+            _lastShakeTime = Time.time;
+        }
+        if (_shakesForExplode == 0)
+            StartCoroutine(Explode());
+        _lastAcceleration = currentAcceleration;
+    }
+
+    private IEnumerator Explode()
+    {
+        _explosion.Play();
+        transform.DOScale(Vector3.zero, _explosion.main.duration * 0.7f);
+        while (DOTween.IsTweening(transform))
+            yield return null;
+        OnExplode?.Invoke();
+        Invoke(nameof(DestroyObj), _explosion.main.duration * 0.3f);
+    }
+
+    private void DestroyObj() => Destroy(gameObject);
+}
+
+
+public interface IInteractable
+{
+    public void Interact(Player interactor);
+}
--- Source/RatageddonAR/Assets/!Scripts/Player.cs
+++ Source/RatageddonAR/Assets/!Scripts/Player.cs
@@ -1,13 +1,30 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(PlaceObjects))]
 public class Player : MonoBehaviour
 {
     public PlaceObjects PlaceObjects => GetComponent<PlaceObjects>();
+    [SerializeField] private InputAction _input;
+    public Transform ItemAnchor => _anchor;
+    [SerializeField] private Transform _anchor;
+    private void Awake()
+    {
+        Application.targetFrameRate = 60;
+        Input.gyro.enabled = true;
+        _input.Enable();
+        _input.performed += Interact;
+    }
 
-
-
-
-
-    
+    private void Interact(InputAction.CallbackContext context)
+    {
+        Debug.Log("INTERACT");
+        Ray ray = Camera.main.ScreenPointToRay(_input.ReadValue<Vector2>());
+        Physics.Raycast(ray, out RaycastHit hit, 3);
+        if (hit.collider != null)
+        {
+            Debug.Log("HIT");
+            hit.collider.GetComponent<IInteractable>()?.Interact(this);
+        }
+    }
 }
