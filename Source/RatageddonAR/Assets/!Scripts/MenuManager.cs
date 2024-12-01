efb5a3d4acfa8c40c585dc7c6a8c198f12df6470
DiFlector <egorgorbachenkov@gmail.com>
2024-12-01 18:27:24 +0300

    Menu

---
+++ Source/RatageddonAR/Assets/!Scripts/MenuManager.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] private GameObject _scoreMenu;
+    
+    public void StartGame()
+    {
+        SceneManager.LoadScene(sceneBuildIndex: 1);
+    }
+    
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+    
+    public void ShowScoreMenu()
+    {
+        _scoreMenu.SetActive(value: true);
+    }
+    
+    public void HideScoreMenu()
+    {
+        _scoreMenu.SetActive(value: false);
+    }
+}
